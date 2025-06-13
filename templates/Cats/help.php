e4fec5ee0010ee145b377697327ac9f2f636bc34
AndreasØstergårdChri <ac@vestjyskmarketing.dk>
2025-06-13 12:47:45 +0200

    - Commented out the email form, since it does not work right now.

--- templates/Cats/help.php
+++ templates/Cats/help.php
@@ -10,46 +10,49 @@
         the actual function definition, the same picture is used multiple times, and the list of functions/functionalities is not very long.
     </p>
     <p class="cats-help__text">
-        One way that you can help, is by sending me more pictures of cats that you think would fit the function definitions, and maybe even come with
+        One way that you can help, is by sending me more pictures of cats, which you think would fit the function definitions, and maybe even come with
         suggestions on what functionalities of PHP/CakePHP should be added to the site!
     </p>
     <p class="cats-help__text">Right now, you can just contact me with an email at my business email:
-        <a href="mailto:andreasbxb@pm.me">andreasbxb@pm.me</a>
+        <a href="mailto:andreas2x2@gmail.com">andreas2x2@gmail.com</a>
+    </p>
+    <p class="cats-help__text">
+        I am also trying to setup an emailing service, so if you have any suggestions, please do contact me!
     </p>
 
-    <?= $this->Form->create(null, [
+<!--    <?php /*= $this->Form->create(null, [
         'type' => 'file', // Allows file uploads
         'url' => ['controller' => 'Cats', 'action' => 'help'], // Use correct controller and action
         'class' => 'cats-help-form'
-    ]) ?>
+    ]) */?>
     <div>
-        <?= $this->Form->control('email_title', [
+        <?php /*= $this->Form->control('email_title', [
             'label' => 'Title',
             'required' => true,
             'class' => 'cats-help-form__input'
-        ]) ?>
+        ]) */?>
     </div>
 
     <div>
-        <?= $this->Form->control('email_description', [
+        <?php /*= $this->Form->control('email_description', [
             'type' => 'textarea',
             'label' => 'Description',
             'required' => true,
             'class' => 'cats-help-form__input'
-        ]) ?>
+        ]) */?>
     </div>
 
     <div>
-        <?= $this->Form->control('email_pictures', [
+        <?php /*= $this->Form->control('email_pictures', [
             'type' => 'file',
             'multiple' => true,
             'accept' => 'image/*',
             'label' => 'Attach Pictures',
             'class' => 'cats-help-form__input'
-        ]) ?>
+        ]) */?>
     </div>
 
-    <?= $this->Form->button('Send Email', ['class' => 'cats-help-form__button']) ?>
-    <?= $this->Form->end() ?>
+    <?php /*= $this->Form->button('Send Email', ['class' => 'cats-help-form__button']) */?>
+    --><?php /*= $this->Form->end() */?>
 
 </div>
