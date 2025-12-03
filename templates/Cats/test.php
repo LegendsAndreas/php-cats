dffc93cab9caaa0441842e5e7b88b628b3236249
AndreasØstergårdChri <ac@vestjyskmarketing.dk>
2025-12-03 15:41:38 +0100

    Bold stuff

--- templates/Cats/test.php
+++ templates/Cats/test.php
@@ -1,5 +1,6 @@
 <?php
 /**
+ * Using the tag "pre" keeps the indentation of the text
  * @var \App\View\AppView $this
  */
 ?>
@@ -7,6 +8,7 @@
 <div>
     <label for="search-input">Text</label>
     <textarea class="js-get-area-text" id="search-input" placeholder="Text..."></textarea>
+    <button class="js-bold-text" type="button">Bold</button>
     <label for="search-type">Type</label>
     <select id="search-type">
         <option value="code">Code</option>
@@ -23,69 +25,45 @@
 </div>
 
 <div class="list-group-item js-code-placeholder code-segment" style="display: none">
-    <button class="js-write delete-button">X</button>
+    <div class="d-flex">
+        <div class="col">
+        <pre class="js-add-text-to-pre">
+        </pre>
+        </div>
+        <div class="col-auto">
+            <button class="js-write delete-button">X</button>
+        </div>
+    </div>
 </div>
+
 <div class="list-group-item js-html-placeholder html-segment" style="display: none">
-    <button class="js-write delete-button">X</button>
+    <div class="d-flex" style="background-color: whitesmoke">
+        <div class="col">
+        <pre class="js-add-text-to-pre">
+        </pre>
+        </div>
+        <div class="col-auto">
+            <button class="js-write delete-button">X</button>
+        </div>
+    </div>
 </div>
+
 <div class="list-group-item js-comment-placeholder comment-segment" style="display: none">
-    <button class="js-write delete-button">X</button>
+    <div class="d-flex">
+        <div class="col">
+        <pre class="js-add-text-to-pre">
+        </pre>
+        </div>
+        <div class="col-auto">
+            <button class="js-write delete-button">X</button>
+        </div>
+    </div>
 </div>
-
-<style>
-    .delete-button {
-        height: unset;
-        padding: 5px;
-        margin-bottom: 5px;
-        display: flex;
-        justify-self: right;
-    }
-
-    .list-group-item {
-        z-index: 10;
-        border: none;
-    }
-
-    .list-group {
-        border: 1px solid lightgray;
-        padding: 5px;
-        background-color: #C93941;
-    }
-
-    pre {
-        padding-left: 2px;
-        font-size: 20px;
-        border: none;
-        background-color: unset;
-    }
-
-    .code-segment {
-        background-color: wheat;
-    }
-
-    .comment-segment {
-        background-color: wheat;
-
-        pre {
-            color: green;
-        }
-    }
-
-    .html-segment {
-        background-color: wheat;
-
-        pre {
-            color: #212529;
-            background-color: whitesmoke;
-        }
-    }
-</style>
 
 <script src="https://cdn.jsdelivr.net/npm/sortablejs@1.15.6/Sortable.min.js"></script>
 <script>
     new Sortable(example1, {
         animation : 150,
-        ghostClass: 'blue-background-class'
     });
 
     document.querySelectorAll('.js-add-text').forEach(element => {
@@ -103,15 +81,13 @@
             newElement.classList.remove(`js-${type}-placeholder`);
             newElement.style.display = 'block';
 
-            // Create a <pre> tag to preserve whitespace
-            const preElement            = document.createElement('pre');
-            preElement.style.whiteSpace = 'pre-wrap'; // Allows wrapping while preserving whitespace
-            if (areaText === 0 ||!areaText) {
+            // Add to pre-tag
+            let preElement              = newElement.querySelector('.js-add-text-to-pre');
+            if (areaText === 0 || !areaText) {
                 preElement.innerHTML = '&nbsp;';
             } else {
-                preElement.textContent = areaText;
+                preElement.innerHTML = areaText;
             }
-            newElement.appendChild(preElement);
 
             // Add eventlistener to button
             addRemoveElementListener(newElement.querySelector('.js-write'));
@@ -120,9 +96,30 @@
         })
     })
 
+    document.querySelectorAll('.js-bold-text').forEach(element => {
+        element.addEventListener('click', () => {
+            const textarea = document.querySelector('.js-get-area-text');
+            const start = textarea.selectionStart;
+            const end = textarea.selectionEnd;
+            const selectedText = textarea.value.substring(start, end);
+
+            if (selectedText.length > 0) {
+                const beforeText = textarea.value.substring(0, start);
+                const afterText = textarea.value.substring(end);
+                const boldText = `<strong>${selectedText}</strong>`;
+
+                textarea.value = beforeText + boldText + afterText;
+
+                // Restore cursor position after the bold markers
+                textarea.focus();
+                textarea.setSelectionRange(start + 2, end + 2);
+            }
+        });
+    });
+
     function addRemoveElementListener(element) {
         element.addEventListener('click', () => {
-            element.parentElement.remove();
+            element.closest('.list-group-item').remove();
         })
     }
 
