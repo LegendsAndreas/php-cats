8c32e74e00f14bb2f5b1c03aceb8d8c580e6003c
LegendsAndreas <andreas3x3@protonmail.com>
2025-12-12 20:35:01 +0100

    Removed the redirect as a query parameter

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -25,12 +25,7 @@ class UsersController extends AppController
         $result = $this->Authentication->getResult();
         // regardless of POST or GET, redirect if user is logged in
         if ($result && $result->isValid()) {
-            $redirect = $this->request->getQuery('redirect', [
-                'controller' => 'Cats',
-                'action' => 'index',
-            ]);
-
-            return $this->redirect($redirect);
+            return $this->redirect(['controller' => 'Cats', 'action' => 'index']);
         }
         // display error if user submitted and authentication failed
         if ($this->request->is('post') && !$result->isValid()) {
